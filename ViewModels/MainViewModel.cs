369b74229e5a4e001ca0ce07a83b73f28ecdbbaa
Viemean <lxf74663@gmail.com>
2025-08-20 03:37:25 +0800

    迁移应用状态保存方法到单独文件

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -17,34 +17,6 @@ using StarResonance.DPS.Services;
 
 namespace StarResonance.DPS.ViewModels;
 
-public class AppState
-{
-    public int ElapsedSeconds { get; init; }
-    public bool IsFightActive { get; init; }
-    public double WindowOpacity { get; init; } = 0.85;
-    public double FontSize { get; init; } = 14;
-    public string FontFamilySource { get; init; } = "Microsoft YaHei";
-    public bool IsSmartIdleModeEnabled { get; init; }
-    public string BackendUrl { get; init; } = "ws://localhost:8989";
-    public int UiUpdateInterval { get; init; } = 500;
-    public string? CultureName { get; init; }
-    public bool PauseOnExit { get; init; } = true;
-
-    public bool PauseOnSnapshot { get; init; } = true;
-
-    //用于保存排序规则
-    public string? SortColumn { get; init; }
-
-    public ListSortDirection SortDirection { get; init; }
-
-    /// 主窗口的位置和大小
-    public double WindowTop { get; init; }
-
-    public double WindowLeft { get; init; }
-    public double WindowHeight { get; init; }
-    public double WindowWidth { get; init; }
-}
-
 /// <summary>
 ///     主窗口的视图模型，负责处理所有UI逻辑、数据状态和与服务的交互。
 /// </summary>
